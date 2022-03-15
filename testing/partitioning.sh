2f2c2768e0108e70393bcc487dddbe1213c186ae
frankebel <git@ebel.page>
2022-03-15 17:12:06 +0100

    add partitioning.sh

---
+++ testing/partitioning.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+printf 'On which drive do you want to install Arch Linux?\n'
+lsblk -d
+
+drive_default="$(lsblk -d | awk '{print $1}' | sed -n '2p')"
+printf "Please choose a drive: [%s] " "$drive_default" 
+read -r drive
+drive="${drive:-$drive_default}"
+
+
+printf "\nThis will create 2 partitions on /dev/%s.\nPartition 1 will be of size 512M and type 'EFI System'.\nPartition 2 will take the rest and be of type 'Linux Filesystem'.\nYou will lose all data on /dev/%s. Are you sure you want to continue? [y/N] " "$drive" "$drive"
+read -r yn
+yn="${yn:-n}"
+case "$yn" in
+	[yY]* )
+ 		fdisk "/dev/$drive" <<- FDISK_CMDS
+		g
+		n
+
+
+		+512M
+		t
+		1
+		n
+
+
+
+		w
+		FDISK_CMDS
+		;;
+esac
+unset yn
