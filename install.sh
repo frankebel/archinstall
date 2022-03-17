b6f790e095d88aaa76d7466ae490dd50cc82d12a
frankebel <git@ebel.page>
2022-03-17 19:27:11 +0100

    add Installation, Configure the system, Reboot

--- install.sh
+++ install.sh
@@ -97,4 +97,110 @@ case "$yn" in
 esac
 unset yn
 
+pacstrap /mnt base linux linux-firmware neovim
 
+# Configure the system
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+printf "Select timezone (Enter for list): [Europe/Vienna] "
+read -r timezone
+timezone="${timezone:-Europe/Vienna}"
+region=""
+while true; do
+	if [ -f "/usr/share/zoneinfo/$timezone" ]; then
+		printf "Confirm timezone: %s [y/N] " "$timezone"
+		read -r yn
+		yn="${yn:-n}"
+		case "$yn" in
+			[yY]* )
+				break
+				;;
+			* )
+				timezone=""
+				region=""
+				;;
+		esac
+	elif ! [ "$region" = "" ] && [ -d "/usr/share/zoneinfo/$region" ]; then
+		ls "/usr/share/zoneinfo/$region"
+		printf "Enter city: "
+		read -r city
+		timezone="$region/$city"
+	else
+		find /usr/share/zoneinfo -maxdepth 1 -type d | awk -F "/" '{print $5}'
+		printf "Enter region: "
+		read -r region
+	fi
+
+done
+unset yn
+
+ls -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
+hwclock --systohc
+
+# Localization
+localization='en_US.UTF-8 UTF-8'
+sed -i "s/^#$localization/$localization/" /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+echo 'KEYMAP=colemak' >> /etc/vconsole.conf
+
+# Network configuration
+printf 'Enter hostname: '
+read -r hostname
+while true; do
+	printf "Is this hostname correct? %s [y/N] " "$hostname"
+	read -r yn
+	yn="${yn:-n}"
+	case "$yn" in
+		[yY]* )
+			break
+			;;
+	esac
+done
+unset yn
+
+echo "$hostname" > /etc/hostname
+pacman -S --noconfirm networkmanager
+systemctl enable --now NetworkManager
+
+printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.0.1\t%s\n" "$hostname" > /etc/hosts
+
+# Initramfs
+mkinitcpio -P
+
+# Root password
+while true; do
+	printf 'Enter root password: '
+	read -r pwd_root
+	printf "Please enter again: "
+	read -r pwd_root2
+	if [ "$pwd_root" = "$pwd_root2" ]; then
+		break
+	fi
+done
+unset pwd_root2
+printf "%s\n%s" "$pwd_root" "$pwd_root" | passwd root
+
+# Boot loader
+case "$(lscpu | grep 'Vendor ID')" in
+	*AuthenticAMD )
+		microcode='amd-ucode'
+		;;
+	*GenuineIntel )
+		microcode='intel-ucode'
+		;;
+	* )
+		printf "Could not find microcode for procesor. Aborting script.\n"
+		exit
+		;;
+esac
+pacman -S "$microcode" grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Reboot
+exit
+umount -R /mnt
+
+printf 'Installation is done\n'
