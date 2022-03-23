b1eece47138bc1f1a0ce29b093f342e240ead645
frankebel <git@ebel.page>
2022-03-23 11:57:28 +0100

    wrap prompts with inputs in functions

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-arch_chroot() {
-	arch-chroot /mnt /bin/bash -c "${1}"
-}
-
 # check if in right directory
 if ! [ -f README.md ]; then
 	echo 'Please change in directory archinstall.'
@@ -18,123 +14,137 @@ fi
 
 timedatectl set-ntp true
 
-# Partition the disks, Format the partititons
-echo 'On which drive do you want to install Arch Linux?'
-lsblk -d
 
-drive_default="$(lsblk -d | grep -E '^nvme|^sd' | awk '{print $1}' | head -n 1)"
-printf "Please choose a drive: [%s] " "$drive_default" 
-read -r drive
-drive="${drive:-$drive_default}"
-
-printf "
-This will partition and format /dev/%s.
-\e[1;31mYou will lose all data on /dev/%s.\e[0m Are you sure? [y/N] " "$drive" "$drive"
-read -r yn
-yn="${yn:-n}"
-case "$yn" in
-	[yY]* )
- 		fdisk "/dev/$drive" <<- FDISK_CMDS
-		g
-		n
+arch_chroot() {
+	arch-chroot /mnt /bin/bash -c "${1}"
+}
 
 
-		+512M
-		t
-		1
-		n
+format_and_partition() {
+	echo 'On which drive do you want to install Arch Linux?'
+	lsblk -d
+
+	drive_default="$(lsblk -d | grep -E '^nvme|^sd' | awk '{print $1}' | head -n 1)"
+	printf "Please choose a drive: [%s] " "$drive_default"
+	read -r drive
+	drive="${drive:-$drive_default}"
+
+	printf "
+	This will partition and format /dev/%s.
+	\e[1;31mYou will lose all data on /dev/%s.\e[0m Are you sure? [y/N] " "$drive" "$drive"
+	read -r yn
+	yn="${yn:-n}"
+	case "$yn" in
+		[yY]* )
+			fdisk "/dev/$drive" <<- FDISK_CMDS
+			g
+			n
+
+
+			+512M
+			t
+			1
+			n
 
 
 
-		w
-		FDISK_CMDS
+			w
+			FDISK_CMDS
 
-		mkfs.fat -F 32 /dev/"${drive}1"
-		mkfs.ext4 /dev/"${drive}2"
-		;;
-esac
-unset yn
+			mkfs.fat -F 32 /dev/"${drive}1"
+			mkfs.ext4 /dev/"${drive}2"
+			;;
+	esac
+	unset yn
+}
 
-# Mount the file systems
-printf "Do you want to mount the partitions? Only enter 'y' if you have the default partitioning. [y/N] "
-read -r yn
-case "$yn" in
-	[yY]* )
-		mount /dev/"${drive}2" /mnt
-		mkdir /mnt/boot
-		mount /dev/"${drive}1" /mnt/boot
-		;;
-esac
-unset yn
 
-# Swap file
-printf "Do you want to create a swap file? Enter size in MiB (0 for none): [2048] "
-while true; do
-	read -r swap_size
-	swap_size="${swap_size:-2048}"
-	if [ "$swap_size" -ge 0 ]; then
-		break
-	else
-		printf "Please enter a number: [2048] "
+mount_volumes() {
+	printf "Do you want to mount the partitions? Only enter 'y' if you have the default partitioning. [y/N] "
+	read -r yn
+	case "$yn" in
+		[yY]* )
+			mount /dev/"${drive}2" /mnt
+			mkdir /mnt/boot
+			mount /dev/"${drive}1" /mnt/boot
+			;;
+	esac
+	unset yn
+}
+
+
+create_swap() {
+	printf "Do you want to create a swap file? Enter size in MiB (0 for none): [2048] "
+	while true; do
+		read -r swap_size
+		swap_size="${swap_size:-2048}"
+		if [ "$swap_size" -ge 0 ]; then
+			break
+		else
+			printf "Please enter a number: [2048] "
+		fi
+	done
+
+	if [ "$swap_size" -gt 0 ]; then
+		dd if=/dev/zero of=/mnt/swapfile bs=1M count="$swap_size"
+		chmod 600 /mnt/swapfile
+		mkswap /mnt/swapfile
+		swapon /mnt/swapfile
 	fi
-done
+}
 
-if [ "$swap_size" -gt 0 ]; then
-	dd if=/dev/zero of=/mnt/swapfile bs=1M count="$swap_size"
-	chmod 600 /mnt/swapfile
-	mkswap /mnt/swapfile
-	swapon /mnt/swapfile
-fi
 
-# Installation
-clear
-printf 'Do you want to update the mirrorlist? (This may take a while) [Y/n] '
-read -r yn
-yn="${yn:-y}"
-case "$yn" in
-	[yY]* )
-		cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
-		reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
-		;;
-esac
-unset yn
+update_mirrolist() {
+	printf 'Do you want to update the mirrorlist? (This may take a while) [Y/n] '
+	read -r yn
+	yn="${yn:-y}"
+	case "$yn" in
+		[yY]* )
+			cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+			reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
+			;;
+	esac
+	unset yn
+}
 
 pacstrap /mnt base linux linux-firmware
 
 # Configure the system
 genfstab -U /mnt >> /mnt/etc/fstab
 
-clear
-printf "Select timezone (Enter for list): "
-read -r timezone
-region=""
-while true; do
-	if [ -f "/usr/share/zoneinfo/$timezone" ]; then
-		printf "Confirm timezone: %s [y/N] " "$timezone"
-		read -r yn
-		yn="${yn:-n}"
-		case "$yn" in
-			[yY]* )
-				break
-				;;
-			* )
-				timezone=""
-				region=""
-				;;
-		esac
-	elif ! [ "$region" = "" ] && [ -d "/usr/share/zoneinfo/$region" ]; then
-		ls "/usr/share/zoneinfo/$region"
-		printf "Enter city: "
-		read -r city
-		timezone="$region/$city"
-	else
-		find /usr/share/zoneinfo -maxdepth 1 -type d | awk -F "/" '{print $5}'
-		printf "Enter region: "
-		read -r region
-	fi
 
-done
-unset yn
+set_time_zone() {
+	printf "Select timezone (Enter for list): "
+	read -r timezone
+	region=""
+	while true; do
+		if [ -f "/usr/share/zoneinfo/$timezone" ]; then
+			printf "Confirm timezone: %s [y/N] " "$timezone"
+			read -r yn
+			yn="${yn:-n}"
+			case "$yn" in
+				[yY]* )
+					break
+					;;
+				* )
+					# clear variables to start from scratch
+					timezone=""
+					region=""
+					;;
+			esac
+		elif ! [ "$region" = "" ] && [ -d "/usr/share/zoneinfo/$region" ]; then
+			ls "/usr/share/zoneinfo/$region"
+			printf "Enter city: "
+			read -r city
+			timezone="$region/$city"
+		else
+			find /usr/share/zoneinfo -maxdepth 1 -type d | awk -F "/" '{print $5}'
+			printf "Enter region: "
+			read -r region
+		fi
+	done
+	unset yn
+}
 
 arch_chroot "ln -sf /usr/share/zoneinfo/$timezone /etc/localtime"
 arch_chroot "hwclock --systohc"
@@ -144,47 +154,56 @@ clear
 sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /mnt/etc/locale.gen
 arch_chroot "locale-gen"
 echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
-clear
-echo '
-Select keyboard layout:
-1) colemak
-2) de-latin1
-3) us'
-while true; do
-	printf 'Enter option: (1,2,3) '
-	read -r keyboard_layout
-	case "$keyboard_layout" in
-		1 )
-			keymap='colemak'
-			break
-			;;
-		2 )
-			keymap='de-latin1'
-			break
-			;;
-		3 )
-			keymap='us'
-			break
-			;;
-	esac
-done
+
+
+set_keyboard_layout() {
+	echo '
+	Select keyboard layout:
+	1) colemak
+	2) de-latin1
+	3) us'
+	while true; do
+		printf 'Enter option: (1,2,3) '
+		read -r keyboard_layout
+		case "$keyboard_layout" in
+			1 )
+				keymap='colemak'
+				break
+				;;
+			2 )
+				keymap='de-latin1'
+				break
+				;;
+			3 )
+				keymap='us'
+				break
+				;;
+		esac
+	done
+}
+
+
 echo "KEYMAP=$keymap" > /mnt/etc/vconsole.conf
 
 # Network configuration
-clear
-while true; do
-	printf 'Enter hostname: '
-	read -r hostname
-	printf "Is this hostname correct? %s [y/N] " "$hostname"
-	read -r yn
-	yn="${yn:-n}"
-	case "$yn" in
-		[yY]* )
-			break
-			;;
-	esac
-done
-unset yn
+
+
+set_hostname() {
+	while true; do
+		printf 'Enter hostname: '
+		read -r hostname
+		printf "Is this hostname correct? %s [y/N] " "$hostname"
+		read -r yn
+		yn="${yn:-n}"
+		case "$yn" in
+			[yY]* )
+				break
+				;;
+		esac
+	done
+	unset yn
+}
+
 
 echo "$hostname" > /mnt/etc/hostname
 arch_chroot "pacman -S --noconfirm networkmanager"
@@ -195,98 +214,105 @@ printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.0.1\t%s\n" "$hostname" > /
 # Initramfs
 arch_chroot "mkinitcpio -P"
 
-# Root password
-clear
-while true; do
-	stty -echo
-	printf '\nEnter root password: '
-	read -r pwd_root
-	printf "\nPlease enter again: "
-	read -r pwd_root2
-	if [ "$pwd_root" = "$pwd_root2" ]; then
-		stty echo
-		break
-	fi
-done
-unset pwd_root2
-arch_chroot "printf '%s\n%s' '$pwd_root' '$pwd_root' | passwd root"
 
-# add user
-while true; do
-	printf 'Add regular user: '
-	read -r user
-	printf "\nIs this username correct? %s [y/N] " "$user"
-	read -r yn
-	yn="${yn:-n}"
-	case "$yn" in
-		[yY]* )
+set_root_password() {
+	while true; do
+		stty -echo
+		printf '\nEnter root password: '
+		read -r pwd_root
+		printf "\nPlease enter again: "
+		read -r pwd_root2
+		if [ "$pwd_root" = "$pwd_root2" ]; then
+			stty echo
 			break
+		fi
+	done
+	unset pwd_root2
+	arch_chroot "printf '%s\n%s' '$pwd_root' '$pwd_root' | passwd root"
+}
+
+
+add_user() {
+	while true; do
+		printf 'Add regular user: '
+		read -r user
+		printf "\nIs this username correct? %s [y/N] " "$user"
+		read -r yn
+		yn="${yn:-n}"
+		case "$yn" in
+			[yY]* )
+				break
+				;;
+		esac
+	done
+	unset yn
+	arch_chroot "useradd -m $user -G wheel"
+	while true; do
+		stty -echo
+		printf '\nEnter user password: '
+		read -r pwd_user
+		printf "\nPlease enter again: "
+		read -r pwd_user2
+		if [ "$pwd_user" = "$pwd_user2" ]; then
+			stty echo
+			break
+		fi
+	done
+	unset pwd_user2
+	arch_chroot "printf '%s\n%s' '$pwd_user' '$pwd_user' | passwd $user"
+
+	# add sudo
+	arch_chroot "pacman -S --noconfirm sudo"
+	sed -i '/^# %wheel ALL=(ALL:ALL) ALL/s/^# //' /mnt/etc/sudoers
+}
+
+
+boot_loader() {
+	case "$(lscpu | grep 'Vendor ID')" in
+		*AuthenticAMD )
+			microcode='amd-ucode'
+			;;
+		*GenuineIntel )
+			microcode='intel-ucode'
+			;;
+		* )
+			printf "Could not find microcode for procesor. Aborting script.\n"
+			exit 1
 			;;
 	esac
-done
-unset yn
-arch_chroot "useradd -m $user -G wheel"
-while true; do
-	stty -echo
-	printf '\nEnter user password: '
-	read -r pwd_user
-	printf "\nPlease enter again: "
-	read -r pwd_user2
-	if [ "$pwd_user" = "$pwd_user2" ]; then
-		stty echo
-		break
-	fi
-done
-unset pwd_user2
-arch_chroot "printf '%s\n%s' '$pwd_user' '$pwd_user' | passwd $user"
+	arch_chroot "pacman -S --noconfirm $microcode grub efibootmgr"
+	arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB"
+	arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
+}
 
-# add sudo
-arch_chroot "pacman -S --noconfirm sudo"
-sed -i '/^# %wheel ALL=(ALL:ALL) ALL/s/^# //' /mnt/etc/sudoers
 
-# Boot loader
-case "$(lscpu | grep 'Vendor ID')" in
-	*AuthenticAMD )
-		microcode='amd-ucode'
-		;;
-	*GenuineIntel )
-		microcode='intel-ucode'
-		;;
-	* )
-		printf "Could not find microcode for procesor. Aborting script.\n"
-		exit 1
-		;;
-esac
-arch_chroot "pacman -S --noconfirm $microcode grub efibootmgr"
-arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB"
-arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
+set_text_editor() {
+	echo '
+	Select text editor:
+	1) nano
+	2) neovim
+	3) vim'
+	while true; do
+		printf 'Enter option: (1,2,3) '
+		read -r editor
+		case "$editor" in
+			1 )
+				editor='nano'
+				break
+				;;
+			2 )
+				editor='neovim'
+				break
+				;;
+			3 )
+				editor='vim'
+				break
+				;;
+		esac
+	done
+	arch_chroot "pacman -S --noconfirm $editor"
+}
 
-# add text editor
-clear
-echo '
-Select text editor:
-1) nano
-2) neovim
-3) vim'
-while true; do
-	printf 'Enter option: (1,2,3) '
-	read -r editor
-	case "$editor" in
-		1 )
-			editor='nano'
-			break
-			;;
-		2 )
-			editor='neovim'
-			break
-			;;
-		3 )
-			editor='vim'
-			break
-			;;
-	esac
-done
-arch_chroot "pacman -S --noconfirm $editor"
 
 # Reboot
 if [ "$swap_size" -gt 0 ]; then
